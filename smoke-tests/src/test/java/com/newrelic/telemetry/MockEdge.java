0aa971543dd122ee53a1e601667e0bf372dcb78b
Xi Xia <xxia@newrelic.com>
2020-12-03 23:04:32 -0800

    google format

--- smoke-tests/src/test/java/com/newrelic/telemetry/MockEdge.java
+++ smoke-tests/src/test/java/com/newrelic/telemetry/MockEdge.java
@@ -7,6 +7,9 @@ package com.newrelic.telemetry;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ArrayNode;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Objects;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import org.slf4j.Logger;
@@ -17,101 +20,115 @@ import org.testcontainers.containers.output.Slf4jLogConsumer;
 import org.testcontainers.containers.wait.strategy.Wait;
 import org.testcontainers.utility.DockerImageName;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.Objects;
-
 /**
- * This class uses a docker image to mock the backend service for our trace and metric api endpoints. During the test the exporter will send data to it.
+ * This class uses a docker image to mock the backend service for our trace and metric api
+ * endpoints. During the test the exporter will send data to it.
  */
 public class MockEdge extends GenericContainer {
 
-    public static final int TIMEOUT_IN_MILLIS = 120_000;
-    protected static OkHttpClient client = OkHttpUtils.client();
-    private static final Logger logger = LoggerFactory.getLogger(MockEdge.class);
-    private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
-    private static final String IMAGE_NAME = "newrelic/mock-edge:testing";
-    private final Network network;
-    private static GenericContainer mockEdge;
-    static final String NETWORK_ALIAS = "backend";
+  public static final int TIMEOUT_IN_MILLIS = 120_000;
+  protected static OkHttpClient client = OkHttpUtils.client();
+  private static final Logger logger = LoggerFactory.getLogger(MockEdge.class);
+  private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
+  private static final String IMAGE_NAME = "newrelic/mock-edge:testing";
+  private final Network network;
+  private static GenericContainer mockEdge;
+  static final String NETWORK_ALIAS = "backend";
 
-    public MockEdge(Network network) {
-        this.network = network;
+  public MockEdge(Network network) {
+    this.network = network;
+  }
+
+  public void start() {
+    mockEdge =
+        new GenericContainer<>(DockerImageName.parse(IMAGE_NAME))
+            .withExposedPorts(8080)
+            .waitingFor(Wait.forHttp("/health").forPort(8080))
+            .withNetwork(network)
+            .withNetworkAliases(NETWORK_ALIAS)
+            .withLogConsumer(new Slf4jLogConsumer(logger));
+    mockEdge.start();
+  }
+
+  private JsonNode waitForJson(String url) throws IOException, InterruptedException {
+    long timeout = System.currentTimeMillis() + TIMEOUT_IN_MILLIS;
+    JsonNode jsonNode = null;
+
+    // try repeatedly if necessary to fetch data
+    while (System.currentTimeMillis() < timeout) {
+      InputStream body =
+          Objects.requireNonNull(
+                  client.newCall(new Request.Builder().url(url).build()).execute().body())
+              .byteStream();
+      jsonNode = OBJECT_MAPPER.readTree(body);
+      if (jsonNode.isEmpty()) {
+        Thread.sleep(500);
+      } else {
+        break;
+      }
     }
+    return jsonNode;
+  }
 
-    public void start() {
-        mockEdge = new GenericContainer<>(
-                DockerImageName.parse(IMAGE_NAME))
-                .withExposedPorts(8080)
-                .waitingFor(Wait.forHttp("/health").forPort(8080))
-                .withNetwork(network)
-                .withNetworkAliases(NETWORK_ALIAS)
-                .withLogConsumer(new Slf4jLogConsumer(logger));
-        mockEdge.start();
+  /**
+   * @return A simplified representation of metrics that have been reported to the backend in json
+   *     format
+   * @throws IOException
+   * @throws InterruptedException
+   */
+  public ArrayNode getMetrics() throws IOException, InterruptedException {
+    String url = String.format("http://localhost:%d/metric/get", mockEdge.getMappedPort(8080));
+    JsonNode jsonNode = waitForJson(url);
+    if (jsonNode == null) {
+      return null;
     }
-
-    private JsonNode waitForJson(String url) throws IOException, InterruptedException {
-        long timeout = System.currentTimeMillis() + TIMEOUT_IN_MILLIS;
-        JsonNode jsonNode = null;
-
-        // try repeatedly if necessary to fetch data
-        while (System.currentTimeMillis() < timeout) {
-            InputStream body = Objects.requireNonNull(client.newCall(
-                    new Request.Builder().url(url).build())
-                    .execute()
-                    .body())
-                    .byteStream();
-            jsonNode = OBJECT_MAPPER.readTree(body);
-            if (jsonNode.isEmpty()) {
-                Thread.sleep(500);
-            } else {
-                break;
-            }
-        }
-        return jsonNode;
+    if (jsonNode.isArray()) {
+      return (ArrayNode) jsonNode;
     }
+    return null;
+  }
 
-    /**
-     * @return A simplified representation of metrics that have been reported to the backend in json format
-     * @throws IOException
-     * @throws InterruptedException
-     */
-    public ArrayNode getMetrics() throws IOException, InterruptedException {
-        String url = String.format("http://localhost:%d/metric/get", mockEdge.getMappedPort(8080));
-        JsonNode jsonNode = waitForJson(url);
-        if (jsonNode == null) {
-            return null;
-        }
-        if (jsonNode.isArray()) {
-            return (ArrayNode) jsonNode;
-        }
-        return null;
+  /**
+   * @return A simplified representation of spans that have been reported to the backend in json
+   *     format
+   * @throws IOException
+   * @throws InterruptedException
+   */
+  public ArrayNode getSpans() throws IOException, InterruptedException {
+    String url = String.format("http://localhost:%d/trace/get", mockEdge.getMappedPort(8080));
+    JsonNode jsonNode = waitForJson(url);
+    if (jsonNode.isArray()) {
+      return (ArrayNode) jsonNode;
     }
+    return null;
+  }
 
-    /**
-     * @return A simplified representation of spans that have been reported to the backend in json format
-     * @throws IOException
-     * @throws InterruptedException
-     */
-    public ArrayNode getSpans() throws IOException, InterruptedException {
-        String url = String.format("http://localhost:%d/trace/get", mockEdge.getMappedPort(8080));
-        JsonNode jsonNode = waitForJson(url);
-        if (jsonNode.isArray()) {
-            return (ArrayNode) jsonNode;
-        }
-        return null;
-    }
+  /**
+   * Clears out all the metrics and spans from the backend service
+   *
+   * @throws IOException
+   */
+  public void clear() throws IOException {
+    client
+        .newCall(
+            new Request.Builder()
+                .url(
+                    String.format(
+                        "http://localhost:%d/metrics/clear", mockEdge.getMappedPort(8080)))
+                .build())
+        .execute()
+        .close();
+    client
+        .newCall(
+            new Request.Builder()
+                .url(
+                    String.format("http://localhost:%d/traces/clear", mockEdge.getMappedPort(8080)))
+                .build())
+        .execute()
+        .close();
+  }
 
-    /**
-     * Clears out all the metrics and spans from the backend service
-     * @throws IOException
-     */
-    public void clear() throws IOException {
-        client.newCall(new Request.Builder().url(String.format("http://localhost:%d/metrics/clear", mockEdge.getMappedPort(8080))).build()).execute().close();
-        client.newCall(new Request.Builder().url(String.format("http://localhost:%d/traces/clear", mockEdge.getMappedPort(8080))).build()).execute().close();
-    }
-
-    public void stop() {
-        mockEdge.stop();
-    }
+  public void stop() {
+    mockEdge.stop();
+  }
 }
